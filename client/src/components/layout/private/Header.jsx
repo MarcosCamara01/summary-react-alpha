da014e36dab03c47d47126f1c4981fae0b99c633
MarcosCamara01 <marcospenelascamara@gmail.com>
2023-06-21 22:37:52 +0200

    dropdown menu

--- client/src/components/layout/private/Header.jsx
+++ client/src/components/layout/private/Header.jsx
@@ -1,16 +1,69 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import avatar from "../../../assets/user.png";
+import { IoIosArrowDown } from 'react-icons/io';
+import { FiUser } from 'react-icons/fi';
+import { FiBarChart2 } from 'react-icons/fi';
+import { FiCreditCard } from 'react-icons/fi';
+import { FiPower } from 'react-icons/fi';
 
 export const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
   return (
-    <div className='header'>
+    <>
+      <div className='header'>
         <header>
-            <div></div>
-            <nav>
-                <Link to={"/"}>Home</Link>
-                <Link to={"/dashboard"}>Dashboard</Link>
-            </nav>
+          <div></div>
+          <nav>
+            <Link to={"/"}>Home</Link>
+            <Link to={"/dashboard"}>Dashboard</Link>
+            <button className='button-menu' onClick={toggleMenu}>
+              <div><img src={avatar} alt="user" /></div>
+              <div><IoIosArrowDown /></div>
+            </button>
+          </nav>
         </header>
-    </div>
-  )
-}
+      </div>
+      {menuOpen && (
+        <div className="dropdown-menu" onBlur={closeMenu}>
+          <div className="menu">
+            <Link to={"#"}>
+              <div className='link-content'>
+                <div><FiUser /></div>
+                Account Settings
+              </div>
+            </Link>
+            <Link to={"#"}>
+              <div className='link-content'>
+                <div><FiBarChart2 /></div>
+                Account Usage
+              </div>
+            </Link>
+            <Link to={"#"}>
+              <div className='link-content'>
+                <div><FiCreditCard /></div>
+                Biling Details
+              </div>
+            </Link>
+            <div className='separator'></div>
+            <Link to={"#"}>
+              <div className='link-content link-red'>
+                <div><FiPower /></div>
+                Logout
+              </div>
+            </Link>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
